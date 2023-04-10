09b9d8b97ff7b17ab6c29e72201b8a2ca22ad558
CamilaAchuri <camilaachury.4@gmail.com>
2023-04-10 12:28:48 -0300

    DSKTCH-23 - usar el paquete dsdataprep para las configuraciones necesarias en los datos

--- R/compat-utils.R
+++ R/compat-utils.R
@@ -11,21 +11,39 @@ data_draw <- function(data,
   col <- geodato::parse_col(data, var)
   data$..var <- data[[col]]
 
-  d <- geodato::gd_match(data, map_name)
+  data <- geodato::gd_match(data, map_name)
   tj <- geodato::gd_tj(map_name)
 
 
   if(!is.null(data)){
+    if (!"..labels" %in% names(data)) {
+      data$..labels <- dsdataprep::prep_tooltip(data = data,
+                                                tooltip = opts$tooltip_template,
+                                                new_labels = NULL,
+                                                engine = "html",
+                                                as_df = FALSE,
+                                                na_row_default_column = NULL,
+                                                na_row_default_value = NULL,
+                                                na_label = opts$na_label,
+                                                format_num = opts$format_sample_num,
+                                                opts_format_num = list(prefix = opts$prefix_num,
+                                                                       suffix = opts$suffix_num,
+                                                                       si_prefix = opts$si_prefix),
+                                                format_cat = opts$format_sample_cat,
+                                                format_date = opts$format_sample_dat)
+    }
     dgeo <- tj |>
-      dplyr::left_join(d, by = c(id = "..gd_id", name = "..gd_name"))
+      dplyr::left_join(data, by = c(id = "..gd_id", name = "..gd_name"))
+
   } else{
-    dgeo <- d
+    dgeo <- tj
   }
 
   dgeo <- dgeo %>% sf::st_set_crs(3857)
   shape_transform <- sf::st_transform(dgeo,
                                       "+proj=mill +lat_0=0 +lon_0=0 +x_0=0 +y_0=0 +R_A +datum=WGS84 +units=m +no_defs")
   shape_json <- geojsonio::geojson_json(shape_transform)
+  shape_json
 
 
 }
